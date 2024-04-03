8b3e8ca837ad13500d6f37eff0e354cb047727c9
Vihang26 <vhpancholi@gmail.com>
2024-04-02 17:32:40 -0700

    dataset added

--- model.py
+++ model.py
@@ -150,7 +150,8 @@ def test():
     x = torch.randn((3, 1, 161, 161))
     model = UNET(in_channels=1, out_channels=1)
     preds = model(x)
-    print(preds)
+    print(preds.shape)
+    print(x.shape)
     assert preds.shape == x.shape
 
 if __name__ == "__main__":
