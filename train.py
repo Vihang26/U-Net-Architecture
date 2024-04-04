afba1d73dc0a97ebeb4aa733e5c2073c5a2284f8
Vihang26 <vhpancholi@gmail.com>
2024-04-04 12:00:50 -0700

    added train and utils

---
+++ train.py
@@ -0,0 +1,114 @@
+import torch
+import albumentations as A
+from albumentations.pytorch import ToTensorV2
+from tqdm import tqdm
+import torch.nn as nn
+import torch.optim as optim
+from model import UNET
+
+# from utils import (
+#     load_checkpoint,
+#     save_checkpoint,
+#     get_loaders,
+#     check_accuracy,
+#     save_predictions_as_imgs,
+# )
+
+# Hyper Parameters
+LEARNING_RATE = 1e-4
+DEVICE = "cuda" if torch.cuda.is_available()  else "cpu"
+BATCH_SIZE = 16
+NUM_EPOCHS = 5
+NUM_WORKERS = 2
+IMAGE_HEIGHT = 160
+IMAGE_WIDTH = 240
+PIN_MEMORY = True
+LOAD_MODEL = False
+TRAIN_IMG_DIR = "data/train_images/"
+TRAIN_MASK_DIR = "data/train_masks/"
+VAL_IMG_DIR = "data/val_images/"
+VAL_MASK_DIR = "data/val_masks/"
+
+def train_fn(loader, model, optimizer, loss_fn, scaler): #Train function is for 1 epoch of training
+    loop = tqdm(loader) #for the progress bar
+
+    for batch_idx, (data, targets) in enumerate(loop):
+        data = data.to(device = DEVICE)
+        targets = targets.float().unsqueeze(1).to(device=DEVICE)
+
+        #Forward
+        with torch.cuda.map.autocast():
+            predictions = model(data)
+            loss = loss_fn(predictions, targets)
+
+
+        #Backward
+        optimizer.zero_grad()
+        scaler.scale(loss).backward()
+        scaler.step(optimizer)
+        scaler.update()
+
+        #Update TQDM
+        loop.set_postfix(loss = loss.item())
+    
+
+
+
+def main():
+    train_transform = A.Compose(
+        [
+            A.Resize(height=IMAGE_HEIGHT, width=IMAGE_WIDTH),
+            A.Rotate(limit=35, p=1.0),
+            A.HorizontalFlip(p=0.5),
+            A.VerticalFlip(p=0.1),
+            A.Normalize(
+                mean=[0.0, 0.0, 0.0],
+                std=[1.0, 1.0, 1.0],
+                max_pixel_value=255.0,
+            ),
+            ToTensorV2(),
+        ],
+    )
+
+    val_transforms = A.Compose(
+        [
+            A.Resize(height=IMAGE_HEIGHT, width=IMAGE_WIDTH),
+            A.Normalize(
+                mean=[0.0, 0.0, 0.0],
+                std=[1.0, 1.0, 1.0],
+                max_pixel_value=255.0,
+            ),
+            ToTensorV2(),
+        ],
+    )
+
+    #Creating the model 
+    # for multi class classification chanege the out channels and change the loss function to cross entropy loss
+    model = UNET(in_channels=3, out_channels=1).to(DEVICE)
+    loss_fn = nn.BCEWithLogitsLoss() #Binary cross entropy
+    optimizer = optim.Adam(model.parameters(), lr = LEARNING_RATE)
+
+    train_loader, val_loader = get_loaders(
+        TRAIN_IMG_DIR,
+        TRAIN_MASK_DIR,
+        VAL_IMG_DIR,
+        VAL_MASK_DIR,
+        train_transform,
+        val_transforms,
+        NUM_WORKERS,
+        PIN_MEMORY
+    )
+
+    scaler = torch.cuda.amp.GradScaler()
+    for epoch in range(NUM_EPOCHS):
+         train_fn(train_loader, model, optimizer, loss_fn, scaler)
+
+    #Save Model
+    
+
+
+
+
+
+if __name__ == "__main__":
+    main()
---
+++ utils.py
@@ -0,0 +1,4 @@
+import torch
+import torchvision
+from dataset import CarvanaDataset
+from torch.utils.data import DataLoader
